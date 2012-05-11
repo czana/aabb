e95ccc550abd00e13a110f52010a23a3ab5669ec
bart <wojtowicz.bartlomiej@gmail.com>
2012-05-11 20:01:38 +0200

    ant fix

--- src/aabb/Canvas.java
+++ src/aabb/Canvas.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.geom.RectangularShape;
 import java.util.ArrayList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
 
 
 public class Canvas extends JPanel {
@@ -14,18 +17,44 @@ public class Canvas extends JPanel {
 
 	public Canvas() {
 		setPreferredSize(new Dimension(800, 600));
-		setBackground(Color.WHITE);
+		setBackground(Color.GREEN);
+
+        bindListeners();
 	}
 
 	public void addFigure(RectangularShape figure) {
 		list.add(figure);
 	}
 
+    private void bindListeners() {
+        MouseSupport mouseSupport = new MouseSupport();
+        this.addMouseListener(mouseSupport);
+        this.addMouseListener(mouseSupport);
+    }
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		
+
 	}
+
+    private class MouseSupport extends MouseAdapter implements MouseMotionListener {
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        public void mouseMoved(MouseEvent e) {
+
+        }
+
+    }
 
 }
